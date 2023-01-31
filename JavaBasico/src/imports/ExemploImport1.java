443af3c2e30e87d2777050fb88575c24008c9af3
Marcos Sanhudo <113375194+marcossanhudo@users.noreply.github.com>
2023-01-31 17:40:39 -0300

    Arquivos para o treinamento de terça-feira, 31 de janeiro de 2023.

---
+++ JavaBasico/src/imports/ExemploImport1.java
@@ -0,0 +1,11 @@
+package imports;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class ExemploImport1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+    }
+}
