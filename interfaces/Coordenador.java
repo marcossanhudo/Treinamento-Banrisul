d55ed32420c7f07606fbc7c123b4f6179755a163
Marcos Sanhudo <113375194+marcossanhudo@users.noreply.github.com>
2023-02-03 17:14:09 -0300

    Arquivos para o treinamento de sexta-feira, 3 de fevereiro de 2023.

---
+++ interfaces/Coordenador.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+public class Coordenador implements Funcionario {
+    public double calcularSalario() {
+        return 0;
+    }
+    
+    public void cumprirCargaHoraria() {}
+    
+    public void andar() {}
+    public void cumprimentarPessoas() {}
+}
